b9417ac1ac27663597bc1643ccbab49bd3a29546
daneozxy <daneozxy@yahoo-inc.com>
2015-01-19 16:27:59 -0800

    Initial Commit

---
+++ TipperOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipperOC
+//
+//  Created by Xiangyu Zhang on 1/19/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
